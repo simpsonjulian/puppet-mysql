be27790f849d075be3df633a2234048ba91ddaff
simpsonjulian <simpsonjulian@gmail.com>
2009-08-16 22:45:28 +0100

    set the root password

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,20 @@
 class mysql {
+  mysql    
   package{ [mysql-client, mysql-server, libmysqlclient15-dev]: ensure => installed }
-}
+  
+  if $mysql_root_password { 
+        
+        exec { "Set MySQL server root password":
+          subscribe => [ Package["mysql-server"], Package["mysql-client"] ],
+          refreshonly => true,
+          unless => "mysqladmin -uroot -p$password status",
+          path => "/bin:/usr/bin",
+          command => "mysqladmin -uroot password $password",
+          require => [ Package["mysql-server"], Package["mysql-client"] ]
+        }
+    }
+    else {
+        warning("no mysql root password given - sorry")
+    }
+  }
+
