976a75fa552230719bdc03b692d9b00ce482c9ce
simpsonjulian <simpsonjulian@gmail.com>
2009-09-06 23:08:38 +0100

    use credentials

--- manifests/database.pp
+++ manifests/database.pp
@@ -3,15 +3,15 @@ define mysql::database($ensure) {
     case $ensure {
         present: {
             exec { "Mysql: create $name db":
-                    command => "/usr/bin/mysql --execute=\"CREATE DATABASE $name\";",
-                    unless => "/usr/bin/mysql --execute=\"SHOW DATABASES;\" | grep '$name'",
+                    command => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"CREATE DATABASE $name\";",
+                    unless => "/usr/bin/mysql-u root -p${mysql_root_password} --execute=\"SHOW DATABASES;\" | grep '$name'",
                     require => Class['mysql']
             }
         }
         absent: {
             exec { "Mysql: drop $name db":
-                    command => "/usr/bin/mysql --execute=\"DROP DATABASE $name\";",
-                    onlyif => "/usr/bin/mysql --execute=\"SHOW DATABASES;\" | grep '$name'",
+                    command => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"DROP DATABASE $name\";",
+                    onlyif => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"SHOW DATABASES;\" | grep '$name'",
                     require => Class['mysql']
             }
         }
@@ -19,4 +19,11 @@ define mysql::database($ensure) {
             fail "Invalid 'ensure' value '$ensure' for mysql::database"
         }
     }
+}
+
+define mysql::user($password) {
+  exec { "Mysql: create $name user":
+                  command = "/usr/bin/echo 'CREATE USER \'${name}\'@\'localhost\' IDENTIFIED BY \'${password}\'; 
+                  GRANT ALL PRIVILEGES ON *.* TO \'monty\'@\'localhost\' WITH GRANT OPTION;"}
+                  onlyif => 
 }
