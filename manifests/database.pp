8215f1abc52d02be7a53063285cc2b89660f5e01
simpsonjulian <simpsonjulian@gmail.com>
2009-11-04 00:06:35 +0000

    am i overusing $name?

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,17 +1,17 @@
-define mysql::database($name, $ensure) {
+define mysql::database($dbname, $ensure) {
 
     case $ensure {
         present: {
-            exec { "Mysql: create $name db":
-                    command => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"CREATE DATABASE $name\";",
-                    unless => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"SHOW DATABASES;\" | grep '$name'",
+            exec { "Mysql: create $dbname db":
+                    command => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"CREATE DATABASE $dbname\";",
+                    unless => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"SHOW DATABASES;\" | grep '$dbname'",
                     require => Class['mysql']
             }
         }
         absent: {
-            exec { "Mysql: drop $name db":
-                    command => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"DROP DATABASE $name\";",
-                    onlyif => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"SHOW DATABASES;\" | grep '$name'",
+            exec { "Mysql: drop $dbname db":
+                    command => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"DROP DATABASE $dbname\";",
+                    onlyif => "/usr/bin/mysql -u root -p${mysql_root_password} --execute=\"SHOW DATABASES;\" | grep '$dbname'",
                     require => Class['mysql']
             }
         }
